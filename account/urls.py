5f4f5c3df27751ea2386f4c3b1e3e3fbca0dbc10
fares-agour <faresabouagour@gmail.com>
2024-04-14 20:51:40 +0200

    add signup form

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,7 @@
-from django.contrib.auth import views as auth_views
 # from django.urls import path, include
 #
 # from . import views
 # from .views import custom_logout_view, CustomPasswordChangeView, CustomPasswordChangeDoneView
-from .views import custom_logout_view
 # app_name = "account"
 # urlpatterns = [
 #     path('', include('django.contrib.auth.urls')),
@@ -16,38 +14,13 @@ from .views import custom_logout_view
 from django.urls import path, include
 
 from . import views
+from .views import custom_logout_view
 
 urlpatterns = [
-    # previous login view
-    # path('login/', views.user_login, name='login'),
-
-    # path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', custom_logout_view, name='logout'),
-
-    # change password urls
-    # path('password-change/',
-    #      auth_views.PasswordChangeView.as_view(),
-    #      name='password_change'),
-    # path('password-change/done/',
-    #      auth_views.PasswordChangeDoneView.as_view(),
-    #      name='password_change_done'),
-
-    # reset password urls
-    # path('password-reset/',
-    #      auth_views.PasswordResetView.as_view(),
-    #      name='password_reset'),
-    # path('password-reset/done/',
-    #      auth_views.PasswordResetDoneView.as_view(),
-    #      name='password_reset_done'),
-    # path('password-reset/<uidb64>/<token>/',
-    #      auth_views.PasswordResetConfirmView.as_view(),
-    #      name='password_reset_confirm'),
-    # path('password-reset/complete/',
-    #      auth_views.PasswordResetCompleteView.as_view(),
-    #      name='password_reset_complete'),
-
     path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
+    path('signup/', views.signup, name="signup")
     # path('register/', views.register, name='register'),
     # path('edit/', views.edit, name='edit'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,93 +1,15 @@
-# from django.contrib.auth import authenticate, login
-# from django.contrib.auth import logout
-# from django.contrib.auth.decorators import login_required
-# from django.http import HttpResponse
-# from django.shortcuts import render, redirect
-# from django.urls import reverse
-# from django.contrib.auth import views as auth_views
-# from .forms import LoginForm
-# from django.urls import reverse_lazy
-# from django.contrib.auth import views as auth_views
-#
-#
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+
+from .forms import SignUpForm
+
+
 def custom_logout_view(request):
     # Handle logout logic here (if needed)
     logout(request)
     # Render the logout.html template
     return render(request, 'registration/logout.html')
-
-
-# def user_login(request):
-#     if request.method == 'POST':
-#         form = LoginForm(request.POST)
-#         if form.is_valid():
-#             cd = form.cleaned_data
-#             user = authenticate(request,
-#                                 username=cd['username'],
-#                                 password=cd['password'])
-#             if user is not None:
-#                 if user.is_active:
-#                     login(request, user)
-#                     return redirect(reverse('account:dashboard'))
-#                 else:
-#                     return HttpResponse('Disabled account')
-#             else:
-#                 render(request, 'registration/login.html', {'form': form})
-#     else:
-#         form = LoginForm()
-#     return render(request, 'registration/login.html', {'form': form})
-#
-#
-#
-#
-# class CustomPasswordChangeView(auth_views.PasswordChangeView):
-#     success_url = reverse_lazy('account:password_change_done')  # replace with your desired success URL
-#     template_name = 'registration/password_change.html'  # replace with your custom template path
-#
-#     def get_success_url(self):
-#         return self.success_url
-#
-#
-# class CustomPasswordChangeDoneView(auth_views.PasswordChangeDoneView):
-#     template_name = 'registration/password_change_done.html'
-#
-#
-#
-#
-# @login_required
-# def dashboard(request):
-#     return render(request,
-#                   'account/dashboard.html',
-#                   {'section': 'dashboard'})
-# --------------------------------------------------------------------------------------
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-from django.contrib import messages
-from .forms import LoginForm
-from .models import Profile
-
-
-def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request,
-                                username=cd['username'],
-                                password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('Authenticated successfully')
-                else:
-                    return HttpResponse('Disabled account')
-            else:
-                return HttpResponse('Invalid login')
-    else:
-        form = LoginForm()
-    return render(request, 'account/login.html', {'form': form})
 
 
 @login_required
@@ -96,3 +18,24 @@ def dashboard(request):
                   'account/dashboard.html',
                   {'section': 'dashboard'})
 
+
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from .forms import SignUpForm  # Adjust this import based on your actual form location
+
+
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+            username = user.username
+            password = form.cleaned_data["password1"]
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("../profile/")
+    else:
+        form = SignUpForm()
+
+    return render(request, "registration/signup.html", {"form": form})
