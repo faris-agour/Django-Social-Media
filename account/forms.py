ce2ce2d07ed821c8fe30cbc69d76b44730109a1a
fares-agour <faresabouagour@gmail.com>
2024-04-15 17:13:26 +0200

    prevent add email is used

--- account/forms.py
+++ account/forms.py
@@ -15,14 +15,26 @@ class SignUpForm(UserCreationForm):
         model = User
         fields = ["username", "first_name", "last_name", "email", "password1", "password2"]
 
+        # prevent existing email
+    def clean_email(self):
+        data = self.cleaned_data["email"]
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError("This email address is already used")
+        return data
+
 
 class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email']
+    def clean_email(self):
+        data = self.cleaned_data["email"]
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError("This email address is already used")
+        return data
 
 
 class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['date_of_birth',"photo"]
+        fields = ['date_of_birth', "photo"]
